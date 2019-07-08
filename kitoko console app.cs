038448a3fff4235b427562d386c2e1c27be8796b
Kitoko Matondo <42552317+kitokomatondo@users.noreply.github.com>
2019-07-08 14:10:05 +0200

    Add files via upload
    console app 1

---
+++ kitoko console app.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I'm a console App made by your colleague Kitoko. \n \n What's your name?");
+            string response = Console.ReadLine();
+            Console.WriteLine($"Hello, {response}! How long have you been working at Kindee?");
+            string response2 = Console.ReadLine();
+            Console.WriteLine($"{response2}? Cool! \n \n  Well i don't wanna bother you {response} \n I know you got alot of work to do. \n \n Have a nice day {response}!");
+        }
+    }
+}
